6b9f0a45a192d3c645834fb39446c82001b46a81
aeclouds <30486012+aeclouds@users.noreply.github.com>
2017-07-27 00:24:59 -0400

    Add files via upload

--- safelink-3-multi.js
+++ safelink-3-multi.js
@@ -21,11 +21,11 @@ if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
 var x=Math.floor((Math.random()*5)+ 1);
 var xxx=null;
-if(x=="1"){xxx="how-to-read-forex-chart-properly.html"}
-if(x=="2"){xxx="google-drive-cloud.html"}
-if(x=="3"){xxx="auto-insurance-quotes-2015.html"}
-if(x=="4"){xxx="ways-to-get-cheapest-health-insuranc.html"}
-if(x=="5"){xxx="stress-eating-helps.html"}a_to_ve[i].href="https://safelink-3.blogspot.com/2016/09/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="1"){xxx="1"}
+if(x=="2"){xxx="2"}
+if(x=="3"){xxx="3"}
+if(x=="4"){xxx="4"}
+if(x=="5"){xxx="5"}a_to_ve[i].href="https://aeclouds.blogspot.com/2017/07/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
